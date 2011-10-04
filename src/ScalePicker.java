80195e8e9fe2a9db2ec52fed29dabd27cd3df3c4
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2011-10-04 01:51:45 +0000

    add copyright (GPLv3)

--- src/ScalePicker.java
+++ src/ScalePicker.java
@@ -1,6 +1,22 @@
 package nz.gen.geek_central.infinirule;
 /*
-    Let the user choose a new scale
+    Infinirule--let the user choose which scales to show.
+
+    Copyright 2011 Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see
+    <http://www.gnu.org/licenses/>.
 */
 
 public class ScalePicker extends android.app.Activity
